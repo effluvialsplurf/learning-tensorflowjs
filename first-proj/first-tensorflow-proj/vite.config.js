86081299a40c8c7b3750e8099fd0cfeb47e28538
eeso <eesowrk@gmail.com>
2025-08-26 21:35:14 -0400

    this should help with the gh-pages building

--- first-proj/first-tensorflow-proj/vite.config.js
+++ first-proj/first-tensorflow-proj/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	base: './projects/learning-tensorflowjs/first-proj/first-tensorflow-proj/',
+	base: './',
 });
--- second-proj-image-recognition/image-recognition-tensorflowjs/vite.config.js
+++ second-proj-image-recognition/image-recognition-tensorflowjs/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: './projects/learning-tensorflowjs/second-proj-image-recognition/image-recognition-tensorflowjs/'
+	base: './'
 })
